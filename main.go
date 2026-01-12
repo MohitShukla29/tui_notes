03548ee21c7cdd3573dc5fe64a470266aef5d8ef
Mohit Shukla <mohitshukla2004.ms@gmail.com>
2026-01-12 12:27:58 +0530

    new file can be added

--- main.go
+++ main.go
@@ -4,31 +4,50 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	msg string
+	textInput          textinput.Model
+	createInputVisible bool
 }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Write the command you want to execute!"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 40
+
 	return model{
-		msg: "🍆",
+		textInput:          ti,
+		createInputVisible: false,
 	}
 }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
+		case "ctrl+n":
+			m.createInputVisible = true
+			return m, nil
+		case "enter":
+			return m, nil
 		}
+
 	}
-	return m, nil
+	if m.createInputVisible == true {
+		m.textInput, cmd = m.textInput.Update(msg)
+	}
+	return m, cmd
 }
 func (m model) View() string {
 	welcome := "Welcome to tui Notes"
@@ -39,8 +58,11 @@ func (m model) View() string {
 		PaddingRight(2).
 		PaddingLeft(2)
 	welc := style.Render(welcome)
-
-	help := fmt.Sprintf("%s\n\nCtrl+N: %s, Ctrl+L: %s, Esc: %s, Ctrl+S: %s, Ctrl+Q: %s", welc, "new file", "list", "back/save", "save", "quit")
+	View := ""
+	if m.createInputVisible {
+		View = m.textInput.View()
+	}
+	help := fmt.Sprintf("%s\n\n%s\n\nCtrl+n: %s, Ctrl+L: %s, Esc: %s, Ctrl+S: %s, Ctrl+Q: %s", welc, View, "new file", "list", "back/save", "save", "quit")
 	return help
 }
 
